89af927d805f6642d01d90fd352cca51052783ad
Qwerty <eabiodun45@gmail.com>
2024-09-18 09:00:32 +0100

    Switch to NextAuth V5

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Providers } from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><Header/>{children}<Footer/></body>
+      <body className={inter.className}><Header/><Providers>{children}</Providers><Footer/></body>
     </html>
   );
 }
