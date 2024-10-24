614acfedc2b1511d1a1dac153591f1573e618e4b
Zeeblink <eabiodun45@gmail.com>
2024-10-24 11:54:22 +0100

    ...

--- console.js
+++
@@ -1,9 +0,0 @@
-const API_KEY = "2ecf0a5ce6b7456e97ff317224c58869"
-
-const fecthRecipe = async () => {
-    const res = await fetch(`https://api.spoonacular.com/recipes/${300}/information?apiKey=${API_KEY}`)
-    
-    const recipe = await res.json()
-    console.log(recipe)
-}
-fecthRecipe()
